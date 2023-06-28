722eac8015e09edf11b230adb83103ee14b4bd0e
AcousticSavant <123994190+RMikhaeal@users.noreply.github.com>
2023-06-28 12:59:39 -0400

    Link click to operation functions

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+//Calculator functions
 function add(a, b) {
   return a + b;
 }
@@ -22,22 +23,83 @@ function divide(a, b) {
 function operate(a, b, operator) {
   
   switch (operator) {
-    case '+':
+    case 'add':
       return add(a, b);
-    case '-':
+    case 'subtract':
       return subtract(a, b);
-    case '*':
+    case 'multiply':
       return multiply(a, b);
-    case '/':
+    case 'divide':
       return divide(a, b);
   }
 }
 
+//Create calculator variables
+let x, y, op;
+
+//Respond to button click
 function reply() {
   const fname = this.id;
-  console.log(window[fname]());
+  
+  if(fname == 'equal') {
+    x = operate(Number(x), Number(y), op).toString();
+    y = undefined;
+    op = undefined;
+    console.log(x, y, op);
+    return;
+  } else if(fname == 'clear') {
+    clear();
+    console.log(x, y, op);
+    return;
+  } else if(fname == 'del') {
+    dlt();
+    console.log(x, y, op);
+    return;
+  }
+
+  assign(fname);
 }
 
+//Assign values to variables
+function assign(name) {
+  if(isNaN(name)) {
+    op = name;
+    console.log(x, y, op);
+    return;
+  }
+
+  if(op != undefined) {
+    y != undefined ? y += name : y = name;
+  } else if(op == undefined) {
+    x != undefined ? x += name : x = name;
+  }
+
+  console.log(x, y, op);
+}
+
+function clear() {
+  x = undefined;
+  y = undefined;
+  op = undefined;
+}
+
+function dlt() {
+  if(y != undefined) {
+    y = y.slice(0, -1);
+    if(y.length == 0) {
+      y = undefined;
+    }
+  } else if(op != undefined && y == undefined) {
+    op = undefined;
+  } else if(op == undefined && y == undefined) {
+    x = x.slice(0, -1);
+    if(x.length == 0) {
+      x = undefined;
+    }
+  }
+}
+
+//Add click listener to all buttons
 const btns = document.querySelectorAll('button')
 
 btns.forEach(function(button) {
